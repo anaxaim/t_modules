bdc5add0b78756b5036da66347d8307301a848c7
Evgenii Michurin <anaxaim@gmail.com>
2023-02-27 17:27:03 +0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+terraform {
+    required_version = ">= 0.13.0"
+    required_providers {
+        edgecenter = {
+            source = "Edge-Center/edgecenter"
+            version = "0.1.5"
+        }
+    }
+}
